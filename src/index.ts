9859f996ace4c1cd3468ef722fc6d680121904f4
faris <farischugthai@gmail.com>
2020-08-02 18:53:35 -0400

    yarn start now displays correct initialization of `todo`

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,17 @@
 // I'm glad it still compiles with a shebang
 
 type Todo = {
-  id: number
-  text: string
-  done: boolean
+  // To prevent a function from modifying it's input, you can use the readonly
+  // keyword in TypeScript.
+  readonly id: number
+  readonly text: string
+  readonly done: boolean
 }
 
-function toggleTodo(todo) {
+// The input to toggleTodo must be Todo as shown by the `: Todo` after the todo.
+// The return type of toggleTodo() must also be Todo.
+// We do this by adding : Todo after the parameter list.
+function toggleTodo(todo: Todo): Todo {
   return {
     id: todo.id,
     text: todo.text,
@@ -20,4 +25,15 @@ const foo: Todo = {
   text: '...',
   done: true
 }
+
+const result = toggleTodo({
+  id: 1,
+  text: '...',
+  done: true
+})
+
+console.log('Expected:')
+console.log(`{ id: 1, text: '...', done: false }`)
+console.log('Actual:')
+console.log(result)
 
