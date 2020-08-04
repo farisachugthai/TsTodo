e884fb026a00120470f1e4b046fb4d68d6dafcd4
faris <farischugthai@gmail.com>
2020-08-04 01:00:00 -0400

    Add notes on mapped types and tsconfig.json

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,26 @@
 #!/usr/bin/env tsc
 // I'm glad it still compiles with a shebang
 
-type Todo = {
-  // To prevent a function from modifying it's input, you can use the readonly
-  // keyword in TypeScript.
-  readonly id: number
-  readonly text: string
-  readonly done: boolean
-}
+// Readonly Types:
+type Todo = Readonly<{
+  /*
+   * To prevent a function from modifying it's input, you can use the readonly
+   * keyword in TypeScript. However, rather than modifying every property, let's
+   * modify the entire type.
+   * This is referred to as a mapped type. Mapped types are functions that
+   * take a TypeScript type that outputs a type.
+   * There are many built-in mapped types (like Required<...>, Partial<...>
+   * You can also create your own Mapped Types.
+  */
+  id: number
+  text: string
+  done: boolean
+}>
 
-// The input to toggleTodo must be Todo as shown by the `: Todo` after the todo.
-// The return type of toggleTodo() must also be Todo.
-// We do this by adding : Todo after the parameter list.
 function toggleTodo(todo: Todo): Todo {
+  // The input to toggleTodo must be Todo as shown by the `: Todo` after the todo.
+  // The return type of toggleTodo() must also be Todo.
+  // We do this by adding : Todo after the parameter list.
   return {
     id: todo.id,
     text: todo.text,
