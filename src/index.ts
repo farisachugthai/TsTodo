6f6eaece53fe879902bb2e2340cd00ea54c21322
faris <farischugthai@gmail.com>
2020-08-06 00:30:28 -0400

    Complete the `completeAll` function

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 #!/usr/bin/env tsc
 // I'm glad it still compiles with a shebang
 
-// Readonly Types:
 type Todo = Readonly<{
   /*
+   * Readonly Types:
    * To prevent a function from modifying it's input, you can use the readonly
    * keyword in TypeScript. However, rather than modifying every property, let's
    * modify the entire type.
@@ -11,10 +11,20 @@ type Todo = Readonly<{
    * take a TypeScript type that outputs a type.
    * There are many built-in mapped types (like Required<...>, Partial<...>
    * You can also create your own Mapped Types.
-  */
+   */
   id: number
   text: string
   done: boolean
+}>
+
+type CompletedTodo = Readonly<{
+  /*
+   * In TypeScript, you can use exact values (like true or false) when specifying
+   * a type. These are called literal types.
+   */
+  id: number
+  text: string
+  done: true
 }>
 
 function toggleTodo(todo: Todo): Todo {
@@ -45,3 +55,14 @@ console.log(`{ id: 1, text: '...', done: false }`)
 console.log('Actual:')
 console.log(result)
 
+// Takes an array of todo items and returns a new array of todos where
+// done is all true. Also let's not modify the array of Todos and simply
+// return a new one so make that input of todos readonly
+function completeAll(
+  todos: readonly Todo[]
+): CompletedTodo[] {
+  return todos.map(todo => ({
+    ...todo,
+    done: true
+  }))
+}
